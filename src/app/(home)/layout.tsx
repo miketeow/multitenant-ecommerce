71fd964d30c56e84a3e7982f44ca398364c15796
miketeow <27071053+miketeow@users.noreply.github.com>
2025-06-02 17:50:08 +0800

    implement home layout

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./footer";
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#F4F4F0]">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeLayout;
